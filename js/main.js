d5f0cee88667fbb82ef349ef0e49be5585f1f763
Mudassar Ali <muda@ciklum.com>
2020-06-08 23:19:41 +0500

    home page updated

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 (function ($) {
     $('.navbar-icon').on('click', function () {
         $('#navbars-menu').toggle();
+        $('.navbar-icon-close').show();
+        $('.navbar-icon').hide();
     });
-    $('.navbars-menu').on('click', function () {
+    $('.navbar-icon-close').on('click', function () {
         $('#navbars-menu').toggle();
+        $('.navbar-icon-close').hide();
+        $('.navbar-icon').show();
     });
-
+    $('.dropdown-toggle').on('click', function () {
+        $(this).toggleClass('open');
+    });
     $(document).ready(function () {
         $('.slider').slick({
             infinite: true,
