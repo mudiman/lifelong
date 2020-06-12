3f209c5ad6b9916869291695ba988f84f4686eec
Mudassar Ali <muda@ciklum.com>
2020-06-12 21:26:54 +0500

    second issues fixed

--- js/main.js
+++ js/main.js
@@ -3,11 +3,13 @@
         $('#navbars-menu').toggle();
         $('.navbar-icon-close').show();
         $('.navbar-icon').hide();
+        disableScroll();
     });
     $('.navbar-icon-close').on('click', function () {
         $('#navbars-menu').toggle();
         $('.navbar-icon-close').hide();
         $('.navbar-icon').show();
+        enableScroll();
     });
     $('.dropdown-toggle').on('click', function () {
         $(this).toggleClass('open');
@@ -30,3 +32,19 @@
     }
 
 })(jQuery);
+
+
+function disableScroll() {
+    // Get the current page scroll position
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+
+        // if any scroll is attempted, set this to the previous value
+        window.onscroll = function() {
+            window.scrollTo(scrollLeft, scrollTop);
+        };
+}
+
+function enableScroll() {
+    window.onscroll = function() {};
+}
